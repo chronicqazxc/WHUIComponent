94939bf18da323fdca32df95acfc0316eb4f6543
Wayne Hsiao <chronicqazxc@gmail.com>
2019-09-19 23:56:27 +0800

    init

---
+++ WHUIComponents/WHUIComponents.h
@@ -0,0 +1,19 @@
+//
+//  WHUIComponents.h
+//  WHUIComponents
+//
+//  Created by Wayne Hsiao on 2019/9/19.
+//  Copyright © 2019 Wayne Hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WHUIComponents.
+FOUNDATION_EXPORT double WHUIComponentsVersionNumber;
+
+//! Project version string for WHUIComponents.
+FOUNDATION_EXPORT const unsigned char WHUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WHUIComponents/PublicHeader.h>
+
+
---
+++ WHUIComponentsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WHUIComponentsDemo
+//
+//  Created by Wayne Hsiao on 2019/9/19.
+//  Copyright © 2019 Wayne Hsiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
